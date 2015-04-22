baabd92131a56698e7dadc2639bab768ea52d093
Raphael Dümig <duemig@in.tum.de>
2015-04-22 22:15:12 +0200

    fixed bug in classifier test routine, gaussian fitting for line detection

--- src/classifier_test.py
+++ src/classifier_test.py
@@ -1,12 +1,12 @@
 from mnist_database import mnist_database
 from sklearn import cross_validation
-from functools import partial
-import multiprocessing
 import numpy
 
+from functools import partial
+import multiprocessing
 
 
-def test_cl(classifier, ct, tr_data=None, ts_data=None):
+def _test_cl(classifier, ct, tr_data=None, ts_data=None):
     if tr_data is None:
         tr_data = ct.training_data
     if ts_data is None:
@@ -42,11 +42,11 @@ class classifier_test:
         self.testing_data = self.db.get_testing_data()
     
     def test_classifier(self, classifier, tr_data=None, ts_data=None):
-        return test_cl(self, classifier, tr_data=tr_data, ts_data=ts_data)
+        return _test_cl(classifier, self, tr_data=tr_data, ts_data=ts_data)
     
     def test_classifiers(self, classifiers, processes=1, tr_data=None, ts_data=None):
         pool = multiprocessing.Pool(processes=processes)
-        partial_test = partial(test_cl, ct=self, tr_data=tr_data, ts_data=ts_data)
+        partial_test = partial(_test_cl, ct=self, tr_data=tr_data, ts_data=ts_data)
         return pool.map(partial_test, classifiers)
     
     def cross_validate_classifier(self, classifier):
@@ -58,25 +58,3 @@ class classifier_test:
         return self.test_classifier(classifier,
                                     tr_data=(images_train, lables_train),
                                     ts_data=(images_test, labels_test))
-    
-# workaround for parallelizing instance methods:
-# http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
-def _pickle_method(method):
-    func_name = method.im_func.__name__
-    obj = method.im_self
-    cls = method.im_class
-    return _unpickle_method, (func_name, obj, cls)
-
-def _unpickle_method(func_name, obj, cls):
-    for cls in cls.mro():
-        try:
-            func = cls.__dict__[func_name]
-        except KeyError:
-            pass
-        else:
-            break
-    return func.__get__(obj, cls)
-
-import copy_reg
-import types
-copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
